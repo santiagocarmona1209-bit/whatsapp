b8b6e8c9723026706fa411d09d3b45155c1f1925
santiagocarmona1209-bit <santiagocarmona1209@gmail.com>
2026-05-17 07:14:58 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+import { webcrypto } from "crypto";
+
+if (!globalThis.crypto) {
+  globalThis.crypto = webcrypto;
+}
 import express from "express";
 import pkg from "@whiskeysockets/baileys";
 import QRCode from "qrcode";
